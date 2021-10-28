813d6d2345cf4abaeb939e208d7acdcd84e1e9c8
gbelda <gbelda56@gmail.com>
2021-10-28 14:00:42 +0200

    Succesfully printed on HTML using VueJS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,9 @@
+//Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 const app = new Vue({
     el: '#root',
     data: {
-
+        name: "Start-Up",
+        image: 'https://asianwiki.com/images/d/d9/Start-Up-CP1.jpg'
     },
 
 })
